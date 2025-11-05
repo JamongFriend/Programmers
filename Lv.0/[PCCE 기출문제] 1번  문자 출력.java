ad77ae0e4e393ec977be06aa88bb531c074d5f19
JamogFrinds <ghrb6769@gmail.com>
2025-11-05 13:54:16 +0900

    feat: Lv.0 문자 출력, 심폐소생술

---
+++ Lv.0/[PCCE 기출문제] 1번  문자 출력.java
@@ -0,0 +1,14 @@
+/**
+ * 문제: [PCCE 기출문제] 1번 / 문자 출력
+ * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/340207
+ * 문제 설명:
+ * 주어진 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = " Let's go! ";
+        System.out.println("3\n 2\n 1");
+        System.out.println(message);
+    }
+}
