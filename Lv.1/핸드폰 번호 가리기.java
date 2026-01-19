de97089e42b6aeac2e71dcae4e7e66fbe0a39705
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2026-01-19 14:16:16 +0900

    feat: Lv.1 제일 작은 수 제거하기

--- Lv.1/핸드폰 번호 가리기.java
+++ Lv.1/핸드폰 번호 가리기.java
@@ -1,26 +1,31 @@
 /**
- * 문제: 문자열 내림차순으로 배치하기
+ * 문제: 핸드폰 번호 가리기
  * 난이도: Lv.1
  * 분류: 연습문제
- * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12916
+ * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12948
  * 문제 설명:
- * 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해
- * 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
- * s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+ * 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
+ * 전화번호가 문자열 phone_number로 주어졌을 때, 
+ * 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
  *
  * 풀이 전략:
  */
 
-import java.util.*;
-
 class Solution {
-    public String solution(String s) {
-        String answer = "";
-        String[] str = s.split("");
-        Arrays.sort(str, Collections.reverseOrder());
-        for(String a : str) {
-            answer += a;
+    public String solution(String phone_number) {
+        int backFourNum = phone_number.length()-4;
+        char[] toArray = phone_number.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < phone_number.length(); i++) {
+            if(i < backFourNum) {
+                sb.append("*");
+            }
+            else {
+                sb.append(toArray[i]);
+            }
         }
-        return answer;
+        return sb.toString();
     }
 }
